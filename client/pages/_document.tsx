355bac00bdf36ed7342dfac9bed5d4b938a3d98c
Tom Nagle <tomanagle@gmail.com>
2020-04-15 20:43:58 +1000

    feat: add manifest to document

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -1,7 +1,7 @@
 import Document, { Head, Main, NextScript } from 'next/document';
 import React from 'react';
 import { ServerStyleSheet } from 'styled-components';
-
+import Manifest from 'next-manifest/manifest';
 // @ts-ignore
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -39,6 +39,14 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
           />
           <link rel="stylesheet" type="text/css" href="/nprogress.css" />
+          <Manifest
+            // path for manifest will be deploying
+            href="/manifest.json"
+            // color for `theme-color`
+            themeColor="#24b5b5"
+            // scale for `viewport` meta tag
+            initialScale="1"
+          />
         </Head>
         <body>
           <Main />
