30fd0e2bf7275d33ea9a491056388cf5bd8610fd
Tom Nagle <tomanagle@gmail.com>
2020-06-07 11:17:19 +1000

    feat: donate

--- client/containers/Winner.tsx
+++ client/containers/Winner.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { Result, Button, Modal } from 'antd';
+import { Result, Button, Modal, Alert } from 'antd';
 import { useRouter } from 'next/router';
 import { useResetGameMutation } from '../generated';
+
 const Winner = ({ winner, players, permalink }) => {
   const [closed, setClosed] = React.useState(false);
   const router = useRouter();
@@ -40,6 +41,21 @@ const Winner = ({ winner, players, permalink }) => {
         title={`The ${String(winner).toUpperCase()} team are the winners`}
         subTitle={`Congratulations ${players[0].name} and ${players[1].name}! Your are the winners of the round.`}
       />
+
+      <Alert
+        message="If you enjoyed playing Codenames online, please consider donating a little as $3 USD to help keep the game alive. I host this game at my own expense and want to be able to continue but I need your support."
+        type="success"
+      />
+      <div style={{ marginTop: '1.25rem', textAlign: 'center' }}>
+        <Button
+          type="primary"
+          href="https://www.buymeacoffee.com/tomn"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          DONATE
+        </Button>
+      </div>
     </Modal>
   );
 };
